7db6e13b43e04cfd845bb7866ab4a4ec93692023
Skuy Xans <73455130+natxanss@users.noreply.github.com>
2020-12-12 12:08:03 +0700

    Update heroku.py

--- userbot/modules/heroku.py
+++ userbot/modules/heroku.py
@@ -187,6 +187,7 @@ async def dyno_usage(dyno):
                 "-> `Sisa Dyno Bulan Ini`:\n"
                 f"    •**{hours} jam - {minutes} menit  "
                 f"-  {percentage}%**\n"
+                "**Jangan BOROS ya BOS**"
                 "╰━━━━━━━━━━━━━━━━━━━━╯"
             )
             await asyncio.sleep(20)
