25c3461534ee017e7a265d262c700e018aa494b6
Skuy Xans <73455130+natxanss@users.noreply.github.com>
2020-12-12 15:23:10 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -25,7 +25,7 @@ async def help(event):
             await event.delete()
     else:
         await event.edit(f"**╭━━━━━━━━━━━━━━━━━━━━━╮**\
-            \n│   Help for [🔥XBOT-REMIX🔥]\
+            \n│   Help for [🔥XMOD-REMIX🔥]\
             \n╰━━━━━━━━━━━━━━━━━━━━━╯ \
             \n╭━━━━━━━━━━━━━━━━━━━━━╮\
             \n│   Untuk melihat lengkap Command\
