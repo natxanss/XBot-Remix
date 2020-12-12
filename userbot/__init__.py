898ed7d8fdb2abc2f21acd47cd7f476dcd6e1ad0
Skuy Xans <73455130+natxanss@users.noreply.github.com>
2020-12-12 15:45:36 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -100,7 +100,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/X-Newbie/XBot-Remix.git")
+    "https://github.com/natxanss/XBot-Remix.git")
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "x-sql-extended")
 
