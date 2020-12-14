538fc199d3f441042a12d763d350d38bf6de2ee1
Skuy Xans <73455130+natxanss@users.noreply.github.com>
2020-12-14 12:24:31 +0700

    Update tag_all.py

--- userbot/modules/tag_all.py
+++ userbot/modules/tag_all.py
@@ -17,7 +17,11 @@ async def all(event):
     chat = await event.get_input_chat()
     async for x in bot.iter_participants(chat, 100):
         mentions += f"[\u2063](tg://user?id={x.id})"
-        mentions += f"[\u2063](tg://user?id={x.id})"
+        mentions += f"[\u2063](tg://user?id={x+100.id})"
+        mentions += f"[\u2063](tg://user?id={x+100.id})"
+        mentions += f"[\u2063](tg://user?id={x+100.id})"
+        mentions += f"[\u2063](tg://user?id={x+100.id})"
+        mentions += f"[\u2063](tg://user?id={x+100.id})"
     await bot.send_message(chat, mentions, reply_to=event.message.reply_to_msg_id)
 
 
